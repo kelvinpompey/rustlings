16988a75d14f5e3a103566739b85f2ae3e127c4b
kelvinpompey <kelvin.pompey@gmail.com>
2026-08-28 21:19:31 -0400

    Saved status

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(mut string, command)| {
+                match command {
+                    Command::Uppercase => string.make_ascii_uppercase(),
+                    Command::Trim => string = string.trim().to_string(),
+                    Command::Append(times) => string.push_str(&"bar".repeat(times)),
+                }
+                string
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -36,8 +48,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
